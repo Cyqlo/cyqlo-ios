a630e563858ee6a742b9200d31063b16990b437c
mhendri <mhendrickson91@me.com>
2017-06-24 12:13:01 -0400

    commit for xcode project with pod files

---
+++ Cyqlo Alpha/Cyqlo Alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cyqlo Alpha
+//
+//  Created by Michael Hendrickson on 6/24/17.
+//  Copyright © 2017 Michael Hendrickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
